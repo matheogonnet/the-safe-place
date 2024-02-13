f9f8b68ac2fe7b12994bc186713052517f822d0f
matheogonnet <matheo.gonnet@yahoo.fr>
2024-02-13 11:59:27 +0100

    fix: eleves and parents pages

--- js/eleves.js
+++ js/eleves.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const contentDiv = document.getElementById('eleves-content');
-    contentDiv.innerHTML = `
-        <h2>Ressources Éducatives pour les Élèves</h2>
-        <p>Cette section contient des vidéos, des articles, et des quiz adaptés à votre niveau d'apprentissage.</p>
-        <!-- Ici, vous pouvez ajouter du contenu dynamique comme des vidéos ou des quiz -->
-    `;
-});
+
