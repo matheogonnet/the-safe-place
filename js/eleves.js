e5a997e7aa77da57954d0a1f5b68d42a56f8ebf5
matheogonnet <matheo.gonnet@yahoo.fr>
2024-03-07 12:42:11 +0100

    feat: adding all the quizzes in the studentPage + style

--- js/eleves.js
+++ js/eleves.js
@@ -1,7 +1,27 @@
 
+//fonction pour afficher les quizz
+function showQuiz(quizNumber) {
+    // Masquer tous les quizz
+    document.querySelectorAll('.quiz-content').forEach(function(quiz) {
+        quiz.style.display = 'none';
+    });
+
+    // Afficher le quizz sélectionné
+    document.getElementById('quiz' + quizNumber).style.display = 'block';
+}
+
+
 // Fonction pour vérifier les réponses du quiz
-function checkQuiz() {
-    const answers = { q1: '1', q2: '1', q3: '1', q4: '1', q5: '1' }; // Les bonnes réponses sont marquées par '1'
+function checkQuiz(quizNumber) {
+    // Objets contenant les bonnes réponses pour chaque quizz
+    const correctAnswers = {
+        1: { q1: '1', q2: '1', q3: '1', q4: '1' },
+        2: { q5: '1', q6: '1', q7: '1', q8: '1' },
+        3: { q9: '1', q10: '1', q11: '1', q12: '1' },
+        4: { q13: '1', q14: '1', q15: '1', q16: '1' }
+    };
+
+    const answers = correctAnswers[quizNumber]; // Récupère les bonnes réponses pour le quizz actuel
     let score = 0;
     const totalQuestions = Object.keys(answers).length;
 
@@ -12,9 +32,12 @@ function checkQuiz() {
         }
     });
 
-    const results = document.getElementById('quiz-results');
+    const results = document.getElementById(`quiz${quizNumber}-results`);
     results.textContent = `Vous avez ${score} sur ${totalQuestions} bonnes réponses.`;
+    results.style.display = 'block'; // Affichez les résultats
 }
+
+
 
 // Listenner pour le bouton le defilement des vidéos
 document.addEventListener('keydown', function(e) {
@@ -62,6 +85,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
         observer.observe(video);
     });
 });
+
+
 
 
 
